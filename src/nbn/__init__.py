45ed0c1e80b6736b1772be5d0eaeb11cfe2bd737
Yass Eltahir <15998949+diabolical-ninja@users.noreply.github.com>
2022-06-04 00:45:03 +1000

    Smarten version import

--- src/nbn/__init__.py
+++ src/nbn/__init__.py
@@ -1,2 +1,7 @@
 """Init for NBN Package."""
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version  # type: ignore
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
